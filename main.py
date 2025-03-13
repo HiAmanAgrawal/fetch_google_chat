c0faa66438c01759458f9201d005e5d79a4e1cdf
Aman Agrawal <amanagrawal@192.168.1.6>
2025-03-13 17:43:40 +0530

    fixed a bug

--- main.py
+++ main.py
@@ -145,4 +145,5 @@ def fetch_messages():
 if __name__ == "__main__":
     from os import environ
 
-    app.run(host="0.0.0.0", port=int(environ.get("PORT", 3232)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)), debug=True)
+
